32b6e344f492416d29598f73ce2957c7f5c524bd
rmmcosta <ricardocosta101085@gmail.com>
2024-01-10 18:23:44 +0000

    feat: exercise 1.11 - unicafe step6

--- part1/unicafe/unicafe/src/App.jsx
+++ part1/unicafe/unicafe/src/App.jsx
@@ -1,51 +1,64 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
 
+const roundWith1Decimal = (number) => Math.round(number * 10) / 10;
+
 const Button = ({ text, onClick }) => <button onClick={onClick}>{text}</button>;
 
-const Feedback = ({ handleGoodClick, handleNewtralClick, handleBadClick }) => {
-  return (
-    <>
-      <h1>Give Feedback</h1>
-      <Button text="Good" onClick={handleGoodClick} />
-      <Button text="Neutral" onClick={handleNewtralClick} />
-      <Button text="Bad" onClick={handleBadClick} />
-    </>
-  );
-};
-
-const Statistics = ({ good, neutral, bad }) => (
+const Feedback = ({ handleGoodClick, handleNewtralClick, handleBadClick }) => (
   <>
-    <h1>Statistics</h1>
-    <StatisticsTable good={good} neutral={neutral} bad={bad} />
-    <StatisticsSummary good={good} neutral={neutral} bad={bad} />
+    <h1>Give Feedback</h1>
+    <Button text="Good" onClick={handleGoodClick} />
+    <Button text="Neutral" onClick={handleNewtralClick} />
+    <Button text="Bad" onClick={handleBadClick} />
   </>
 );
 
-const StatisticsTable = ({ good, neutral, bad }) => {
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  const average = (good - bad) / total;
+  const positive = (good / total) * 100;
   return (
     <>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <h1>Statistics</h1>
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticsSummary
+            total={total}
+            average={average}
+            positive={positive}
+          />
+        </tbody>
+      </table>
     </>
   );
 };
 
-const StatisticsSummary = ({ good, neutral, bad }) => {
-  const total = good + neutral + bad;
-  const average = (good - bad) / total;
-  const positive = (good / total) * 100;
+const StatisticLine = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+);
 
-  if (total === 0) return <p>No feedback given</p>;
-  else
+const StatisticsSummary = ({ total, average, positive }) => {
+  if (total === 0) {
     return (
-      <>
-        <p>all: {total}</p>
-        <p>average: {average}</p>
-        <p>positive: {positive}%</p>
-      </>
+      <tr>
+        <td>No feedback given</td>
+      </tr>
     );
+  }
+  return (
+    <>
+      <StatisticLine text="all" value={total} />
+      <StatisticLine text="average" value={roundWith1Decimal(average)} />
+      <StatisticLine text="positive" value={roundWith1Decimal(positive) + "%"} />
+    </>
+  );
 };
 
 const App = () => {
@@ -59,14 +72,14 @@ const App = () => {
   const handleBadClick = () => setBad(bad + 1);
 
   return (
-    <div>
+    <>
       <Feedback
         handleGoodClick={handleGoodClick}
         handleNewtralClick={handleNewtralClick}
         handleBadClick={handleBadClick}
       />
       <Statistics good={good} neutral={neutral} bad={bad} />
-    </div>
+    </>
   );
 };
 
