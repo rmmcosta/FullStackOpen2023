03a2336daee9450b5f531433d77bdfb85d089f74
rmmcosta <ricardocosta101085@gmail.com>
2024-01-15 19:03:44 +0000

    feat: exercise 1.2

--- part2/course-info/src/components/Content.jsx
+++ part2/course-info/src/components/Content.jsx
@@ -1,14 +1,17 @@
 /* eslint-disable react/prop-types */
-import Part from './Part';
+import Total from "./Total";
+import Part from "./Part";
 
 const Content = ({ parts }) => {
+  const total = parts.reduce((acc, initial) => acc + initial.exercises, 0);
   return (
     <>
       {parts.map((part) => (
         <Part key={part.id} part={part} />
       ))}
+      <Total total={total} />
     </>
   );
-}
+};
 
 export default Content;
---
+++ part2/course-info/src/components/Total.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+const Total = ({ total }) => {
+  return (
+    <p>
+      <strong>total of {total} exercises</strong>
+    </p>
+  );
+};
+
+export default Total;
